fc9fecea4670b168d77a3a8c09630f74e3fad489
Dann Luciano <dannluciano@gmail.com>
2017-07-19 15:26:41 -0300

    Fix #10

--- SendIT/plataforma/static/js/app.js
+++ SendIT/plataforma/static/js/app.js
@@ -2,26 +2,20 @@
 /* global ace */
 
 jQuery(document)
-  .ready(() => {
-    var editor = ace.edit('editor')
-    editor.setTheme('ace/theme/github')
-    editor.getSession()
-      .setMode('ace/mode/javascript')
-    editor.setFontSize(20)
+    .ready(function () {
+        var editor = ace.edit('editor');
+        editor.setTheme('ace/theme/github');
+        editor.getSession().setMode('ace/mode/javascript');
+        editor.setFontSize(20);
 
-    var textarea = $('textarea[name="editor"]')
-    textarea.val(editor.getSession()
-      .getValue())
+        var textarea = $('textarea[name="editor"]');
+        textarea.val(editor.getSession().getValue());
 
-    editor.getSession()
-      .on('change', () => {
-        textarea.val(editor.getSession()
-          .getValue())
-      })
+        editor.getSession().on('change', function () {
+            textarea.val(editor.getSession().getValue())
+        });
 
-    $('#botao-executar')
-      .click((evt) => {
-        $('#code')
-          .submit()
-      })
-  })
+        $('#botao-executar').click(function (evt) {
+            $('#code').submit()
+        });
+    });
