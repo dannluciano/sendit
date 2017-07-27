2aba52c35a36761023f33962ed800565a8804e5e
gabrielloliveira <gabrielloliveira097@gmail.com>
2017-07-27 10:14:21 -0300

    Melhorias no cadastro de usuário

--- SendIT/plataforma/views.py
+++ SendIT/plataforma/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from random import randint
 
 
 def index(request):
