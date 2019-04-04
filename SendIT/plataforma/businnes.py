d6e30c3147d9c700d5a23c2a034177278ef01c4d
Dann Luciano <dannluciano@gmail.com>
2019-04-04 10:18:55 -0300

    Add Submission Runner for Java

--- SendIT/plataforma/businnes.py
+++ SendIT/plataforma/businnes.py
@@ -3,22 +3,28 @@ import tempfile
 import logging
 import os
 import shlex
+import shutil
+
 from django.conf import settings
 
 
 L = logging.getLogger('SubmissionRunner')
+
+settings.configure(DEBUG=True, BASE_DIR=os.getcwd())
+
+log_file_path = 'temp/submission_runner.log'
 if settings.DEBUG:
-    L.addHandler(logging.FileHandler('submission_runner.log'))
+    L.addHandler(logging.FileHandler(log_file_path))
     L.setLevel(logging.DEBUG)
 else:
-    L.addHandler(logging.RotatingFileHandler(
-        'submission_runner.log',
+    L.addHandler(logging.handlers.RotatingFileHandler(
+        log_file_path,
         maxBytes=1024 * 1024))
 
 
-def create_temp_file(content, temp_dir='', prefix='', suffix='.txt'):
-    file = tempfile.NamedTemporaryFile(
-        'w+', dir=temp_dir, delete=False, prefix=prefix, suffix=suffix)
+def create_temp_file(content, prefix='', suffix='.txt'):
+    filename = '{}{}'.format(prefix, suffix)
+    file = open(filename, 'w+')
     file.write(content)
     file.seek(0)
     L.info(f'Created Temporary File: {file.name}')
@@ -26,73 +32,150 @@ def create_temp_file(content, temp_dir='', prefix='', suffix='.txt'):
     return file
 
 
-def run_submission(code='', expected_input='', expected_output=''):
+def run_submission(id=0, code='', input='', expected_output=''):
     L.info('SubmissionRunner Started...')
     result = 'OK'
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    
+    tmp_dir = 'temp/submission_{}'.format(id)
+    try:
         os.chdir(tmp_dir)
-        pwd = os.getcwd()
-        L.info(f'Created Temporary Changed Working Dir: {pwd}')
+    except FileNotFoundError as e:
+        os.mkdir(tmp_dir)
+        os.chdir(tmp_dir)
+    
+    pwd = os.getcwd()
+    L.info(f'Created Temporary Changed Working Dir: {pwd}')
 
-        code_file = create_temp_file(code, tmp_dir, 'code_', '.js')
+    code_file = create_temp_file(code, 'Principal', '.java')
 
-        input_file = create_temp_file(
-            expected_input, tmp_dir, 'input_', '.txt')
+    input_file = create_temp_file(
+        input, 'input', '.txt')
 
-        output_file = create_temp_file(
-            expected_output, tmp_dir, 'output_', '.txt')
+    output_file = create_temp_file(
+        expected_output, 'output', '.txt')
 
-        node_command = f'node -i {code_file.name}'
+    command = f'javac {code_file.name}'
 
-        L.info(f'Executing Node Command: {node_command}')
-        outs = b''
+    L.info(f'Executing Command: {command}')
+    process = subprocess.run(shlex.split(command))
+
+    command = f'java Principal'
+
+    L.info(f'Executing Command: {command}')
+    outs = b''
+    try:
+        if input != '':
+            process = subprocess.Popen(
+                shlex.split(command),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE)
+
+            outs, errs = process.communicate(
+                input=input.encode(),
+                timeout=1)
+        else:
+            process = subprocess.run(
+                shlex.split(command),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=True, timeout=1)
+
+            outs = process.stdout
+            errs = process.stderr
+
+    except subprocess.TimeoutExpired as error:
+        result = 'JSTimeoutError'
+        L.error('Timeout Error:')
+    except subprocess.CalledProcessError as error:
+        result = 'JSSintaxError'
+        L.error('SintaxError: ')
+
+    result_file = create_temp_file(
+        outs.decode('utf8'), 'result', '.txt')
+
+    if result == 'OK':
+        diff_command = f'diff -E -b -w -B {output_file.name} {result_file.name}'
+        L.info(f'Executing Diff Command: {diff_command}')
         try:
-            if expected_input != '':
-                process = subprocess.Popen(
-                    shlex.split(node_command),
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    stdin=subprocess.PIPE)
-
-                outs, errs = process.communicate(
-                    input=expected_input.encode(),
-                    timeout=1)
-            else:
-                process = subprocess.run(
-                    shlex.split(node_command),
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    check=True, timeout=1)
-
-                outs = process.stdout
-                errs = process.stderr
-
-        except subprocess.TimeoutExpired as error:
-            result = 'JSTimeoutError'
-            L.error('Timeout Error:')
+            subprocess.run(
+                shlex.split(diff_command), stdout=subprocess.PIPE, check=True)
         except subprocess.CalledProcessError as error:
-            if b'SyntaxError:' in error.stderr:
-                result = 'JSSintaxError'
-                L.error('SintaxError: ')
-            else:
-                result = 'JSRuntimeError'
-                L.error(f'RuntimeError: ')
-
-        result_file = create_temp_file(
-            outs.decode('utf8'), tmp_dir, 'result_', '.txt')
-
-        if result == 'OK':
-            diff_command = f'diff -E -b -w -B {output_file.name} {result_file.name}'
-            L.info(f'Executing Diff Command: {diff_command}')
-            try:
-                subprocess.run(
-                    shlex.split(diff_command), stdout=subprocess.PIPE, check=True)
-            except subprocess.CalledProcessError as error:
-                result = 'DiffError'
-                L.error('Diff Error: Expected Output != Computed Output')
-                L.debug(error.stdout.decode('utf8'))
-                L.debug(outs.decode('utf8'))
-
+            result = 'DiffError'
+            L.error('Diff Error: Expected Output != Computed Output')
+            L.debug(error.stdout.decode('utf8'))
+            L.debug(outs.decode('utf8'))
+    
     os.chdir(settings.BASE_DIR)
+    if not settings.DEBUG:
+        rmtree(temp_dir_name)
+
     L.info('-' * 80)
     return result
+
+
+if __name__ == "__main__":
+    code = """
+class Principal {
+    public static void main(String args[]) {
+        System.out.println("Ola Mundo");
+    }
+}
+"""
+    input = """"""
+    expected_output = """Ola Mundo"""
+
+    print(run_submission(id=1, code=code, input=input, expected_output=expected_output))
+
+
+    code = """
+class Principal {
+    public static void main(String args[]) {
+        while(true){
+
+        }
+    }
+}
+"""
+    input = """"""
+    expected_output = """Ola Mundo"""
+
+    print(run_submission(id=2, code=code, input=input, expected_output=expected_output))
+
+    code = """
+class Principal {
+    public static void main(String args[]) {
+        System.out.println("Ola Mundo")
+    }
+}
+"""
+    input = """"""
+    expected_output = """Ola Mundo"""
+
+    print(run_submission(id=3, code=code, input=input, expected_output=expected_output))
+
+    code = """
+import java.util.Scanner;    
+class Principal {
+    public static void main(String args[]) {
+        Scanner scanner = null;
+        int i = scanner.nextInt();
+    }
+}
+"""
+    input = """"""
+    expected_output = """Ola Mundo"""
+
+    print(run_submission(id=4, code=code, input=input, expected_output=expected_output))
+
+    code = """
+class Principal {
+    public static void main(String args[]) {
+        System.out.println("Oi");
+    }
+}
+"""
+    input = """"""
+    expected_output = """Ola Mundo"""
+
+    print(run_submission(id=5, code=code, input=input, expected_output=expected_output))
