161eaa5c722c67bddcf0133957f1f5c9c01c217b
Dann Luciano <dannluciano@gmail.com>
2019-04-24 09:53:48 -0300

    Add Initial Support for django-session-security

--- SendIT/SendIT/urls.py
+++ SendIT/SendIT/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('plataforma.urls')),
+    url(r'session_security/', include('session_security.urls')),
 ]
