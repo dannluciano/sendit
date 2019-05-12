c07c4d6937f2d53376f31a9c07a65b9463767f6f
Dann Luciano <dannluciano@gmail.com>
2019-05-12 20:14:25 -0300

    Add postgres timeout for 2 seconds

--- SendIT/SendIT/wsgi.py
+++ SendIT/SendIT/wsgi.py
@@ -7,6 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
+
+@receiver(connection_created)
+def setup_postgres(connection, **kwargs):
+    if connection.vendor != 'postgresql':
+        return
+    
+    # Timeout statements after 30 seconds.
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SET statement_timeout TO 20000;
+        """)
+
 import os
 
 from django.core.wsgi import get_wsgi_application
