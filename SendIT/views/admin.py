674c9090c67da933ec80e402c10e3200e9e380f6
Dann Luciano <dannluciano@gmail.com>
2019-06-10 12:25:00 -0300

    Add Submission Summary to App Views

--- SendIT/views/admin.py
+++ SendIT/views/admin.py
@@ -1,24 +1,31 @@
 from django.contrib import admin
 
-from .models import LeaderboardView, StatisticsView
+from .models import LeaderboardView, StatisticsView, SubmissionSummaryView
 
 
-class LeaderboardViewAdmin(admin.ModelAdmin):
-    list_display = ('position', 'username', 'xp')
+class ViewAdmin(admin.ModelAdmin):
+    def get_actions(self, request):
+        return None
 
     def has_add_permission(self, request):
         return False
+
+
+class LeaderboardViewAdmin(ViewAdmin):
+    list_display = ('position', 'username', 'xp')
 
     ordering = ('-xp', )
 
 
-class StatisticsViewAdmin(admin.ModelAdmin):
+class StatisticsViewAdmin(ViewAdmin):
     list_display = ('username', 'level', 'xp', 'number_of_submissions_ok', 'number_of_submission', 'conclusion_rate',
                     'sucess_rate', 'sintax_error_rate', 'runtime_error_rate', 'timeout_error_rate', 'diff_error_rate')
 
-    def has_add_permission(self, request):
-        return False
+
+class SubmissionSummaryViewAdmin(ViewAdmin):
+    list_display = ('status', 'sum')
 
 
 admin.site.register(LeaderboardView, LeaderboardViewAdmin)
 admin.site.register(StatisticsView, StatisticsViewAdmin)
+admin.site.register(SubmissionSummaryView, SubmissionSummaryViewAdmin)
