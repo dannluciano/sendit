13d5989e31c99eef5a784ce9b520a976961e7b3e
Dann Luciano <dannluciano@gmail.com>
2019-06-19 10:34:15 -0300

    Add melhorias no admin

--- SendIT/SendIT/urls.py
+++ SendIT/SendIT/urls.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 
+admin.site.site_header = "SendIt Administração"
+admin.site.site_title = "SendIt"
+admin.site.index_title = "SendIt"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("plataforma.urls", namespace="plataforma")),
@@ -12,4 +16,5 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
