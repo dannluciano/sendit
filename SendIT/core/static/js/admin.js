ddceb6d7e43dc125c553920b648518da8965dfbe
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-06-22 20:21:35 -0300

    added new app core

---
+++ SendIT/core/static/js/admin.js
@@ -0,0 +1,33 @@
+/* eslint-env browser, jquery */
+/* global django, ace */
+
+(function ($) {
+    function setupAce(field) {
+        var editor = ace.edit(`id_${field}`);
+        editor.setTheme('ace/theme/github');
+        editor.getSession()
+            .setMode('ace/mode/javascript');
+        editor.setFontSize(16);
+
+        var textarea = $('<textarea></textarea>', {
+            id: `id_${field}`,
+            style: 'visibility: hidden; display: none',
+            name: `${field}`
+        }).appendTo(`.field-${field}`);
+
+        editor.getSession()
+            .on('change', function () {
+                textarea.val(editor.getSession()
+                    .getValue())
+            });
+
+        editor.insert('\n')
+    }
+
+    $(function () {
+        setupAce('pre_codigo');
+        setupAce('pos_codigo');
+
+        $('pre.ace_editor').css('height', '800px')
+    })
+})(django.jQuery)
