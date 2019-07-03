5fac53d6291d14286f8f3a66841aaf8eccdff75d
Dann Luciano <dannluciano@gmail.com>
2019-07-03 18:55:18 -0300

    Update SendIT/core/urls.py

--- SendIT/core/urls.py
+++ SendIT/core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='platform/index.html', redirect_authenticated_user=True, redirect_field_name = '/home/'), name='index'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'),  name='logout'),
     path('home/', views.home, name="home"),
-    path('random/', views.random_question, name="random"),
+    path('question-random/', views.random_question, name="random"),
     path('completed-issues/', views.completed_issues, name="completed-question"),
     path('question/<int:question_id>/submit/', views.create_submission, name='submit'),
     path('question/<int:question_id>/', views.get_question, name="get-question"),
