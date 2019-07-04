4eb53604b739025840c7e39fbf6299c12888194a
dannluciano <dannluciano@gmail.com>
2019-07-04 13:45:20 -0300

    Fix little bugs

--- SendIT/core/urls.py
+++ SendIT/core/urls.py
@@ -5,13 +5,16 @@ from django.contrib.auth import views as auth_views
 app_name = "core"
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='platform/index.html', redirect_authenticated_user=True, redirect_field_name = '/home/'), name='index'),
+    path('', auth_views.LoginView.as_view(template_name='platform/index.html',
+                                          redirect_authenticated_user=True, redirect_field_name='/home/'), name='index'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'),  name='logout'),
     path('home/', views.home, name="home"),
-    path('question-random/', views.random_question, name="random"),
-    path('questions-completed/', views.completed_issues, name="completed-question"),
-    path('questions/<int:question_id>/submit/', views.create_submission, name='submit'),
-    path('questions/<int:question_id>/', views.get_question, name="get-question"),
+    path('questions/random/', views.random_question, name="random"),
+    path('questions/completed/', views.completed_issues, name="completed-question"),
+    path('questions/<int:question_id>/submit/',
+         views.create_submission, name='submit'),
+    path('questions/<int:question_id>/',
+         views.get_question, name="get-question"),
     path('signup/', views.signup, name="signup"),
     path('ranking/', views.medal_board, name="ranking"),
     path('submissions/', views.submissions_list, name="submissions")
