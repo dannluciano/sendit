1a29669ef19d2fe5fe23442bdf2bccb36a2ee64e
Dann Luciano <dannluciano@gmail.com>
2020-04-22 22:58:42 -0300

    Fix bug when the first test cases are right.

--- core/worker.py
+++ core/worker.py
@@ -5,15 +5,21 @@ from .submission_runner import SubmissionRunnerManager
 def run_submission_runner(submission_id):
     submission = Submission.objects.get(id=submission_id)
     test_cases = CaseTest.objects.filter(question_id=submission.question_id)
+    last_status = submission.status
+    last_log = ''
+    last_output = ''
     for ct in test_cases:
         work_dir = f"{submission.id}/{ct.id}"
         submission_result = SubmissionRunnerManager().exe(
             submission.language, work_dir, ct.sample_input, ct.sample_output, submission.code
         )
-        submission.status = submission_result['status']
-        submission.log = submission_result['log']
-        submission.output = submission_result['output']
-        submission.save()
-        if submission.status != "OK":
+        last_status = submission_result['status']
+        last_log = submission_result['log']
+        last_output = submission_result['output']
+        if last_status != "OK":
             break
+    submission.status = last_status
+    submission.log = last_log
+    submission.output = last_output
+    submission.save()
     return submission.status
