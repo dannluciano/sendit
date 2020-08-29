a346c1a50b56af1aa7644fe271b13fc8289d09d4
Dann Luciano <dannluciano@gmail.com>
2020-08-29 02:17:01 -0300

    Add miniexplorer

--- SendIT/settings.py
+++ SendIT/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "ckeditor",
     "explorer",
     "core",
+    "miniexplorer"
 ]
 
 MIDDLEWARE = [
@@ -73,7 +74,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
         },
     }
 ]
