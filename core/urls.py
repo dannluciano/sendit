a0ce94de42ee0753f8e8e68862b2f922428fe097
Dann Luciano <dannluciano@gmail.com>
2022-01-19 14:32:32 -0300

    Fix Bug with AuthView not respect next param

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ app_name = "core"
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='platform/index.html',
-                                          redirect_authenticated_user=True, redirect_field_name='/home/'), name='index'),
+                                          redirect_authenticated_user=True, redirect_field_name='next'), name='index'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'),  name='logout'),
     path('home/', views.home, name="home"),
     path('questions/random/', views.random_question, name="random"),
