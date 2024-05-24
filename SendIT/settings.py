78a67f20eaa4f44c45731fb5fae0d8218e8e3bb5
Dann Luciano <dannluciano@gmail.com>
2024-05-23 22:13:32 -0300

    Update settings.py

--- SendIT/settings.py
+++ SendIT/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/assets/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 MEDIA_URL = "/uploads/"
 
